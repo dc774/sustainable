16638fea5396fb73df9391f29bfe93d8098b1167
David Cutri <dc774@cornell.edu>
2018-06-26 15:11:31 -0400

    rwd css

--- js/sustainable.js
+++ js/sustainable.js
@@ -5,13 +5,26 @@ jQuery(document).ready(function($) {
 	// Window Size Tracking
 	function resizeChecks() {
 		
-		if ( $(window).width() <= 752 ) {
-			
+		// Mobile navigation
+		if (window.matchMedia('(max-width: 767px)').matches) {
+			$('#navbar .navbar-header').before( $('#navbar .menu.menu--main.nav.navbar-nav') );
+			$('#top-menu .menu--secondary').appendTo('#navbar > .container-fluid > .menu--main');
+			$('#navbar').css('display', 'none');
+			$('#navbar .navbar-header').css('display', 'none');
+			$('#mobile-nav').css('display', 'block');
+			$('#mobile-nav-dimmer').addClass('closed');
+			$('body').removeClass('scrolled');
 		} 
 		else {
-
+			$('#navbar .menu.menu--main.nav.navbar-nav').appendTo('#block-sustainable-main-menu');
+			$('.menu--main .menu--secondary').appendTo('#block-secondary-2');
+			$('#navbar').css('display', 'block');
+			$('#navbar .navbar-header').css('display', 'block');
+			$('#mobile-nav').css('display', 'none');
+			$('#mobile-nav-dimmer').removeClass('open');
+			$('#mobile-nav-dimmer').css('display','none');
 		}
-		
+
 	}
 	
 	var pathArray = window.location.pathname.split( '/' );	
@@ -31,19 +44,34 @@ jQuery(document).ready(function($) {
 	$('#navbar li a').removeClass('dropdown-toggle');
 	
 	// Mobile navigation
-	if ( $(window).width() <= 752 ) {
+	if (window.matchMedia('(max-width: 767px)').matches) {
 		$('#navbar .navbar-header').before( $('#navbar .menu.menu--main.nav.navbar-nav') );
+		$('#top-menu .menu--secondary').appendTo('#navbar > .container-fluid > .menu--main');
+		$('#navbar').css('display', 'none');
 		$('#navbar .navbar-header').css('display', 'none');
-	} else {
-		$('#navbar .menu.menu--main.nav.navbar-nav').appendTo('#block-cuenergy-main-menu');
+		$('#mobile-nav').css('display', 'block');
+		$('#mobile-nav-dimmer').addClass('closed');
+		$('body').removeClass('scrolled');
+	} 
+	else {
+		$('#navbar .menu.menu--main.nav.navbar-nav').appendTo('#block-sustainable-main-menu');
+		$('.menu--main .menu--secondary').appendTo('#block-secondary-2');
+		$('#navbar').css('display', 'block');
 		$('#navbar .navbar-header').css('display', 'block');
+		$('#mobile-nav').css('display', 'none');
+		$('#mobile-nav-dimmer').removeClass('open');
+		$('#mobile-nav-dimmer').css('display','none');
 	}
-	
+
 	$('#mobile-nav').on('click', function() {
 		$('#mobile-nav-dimmer').toggleClass('open closed');
 		$('#navbar').slideToggle('fast');
 	});
-	
+	$('#mobile-nav-dimmer').on('click', function() {
+		$('#mobile-nav-dimmer').toggleClass('open closed');
+		$('#navbar').slideToggle('fast');
+	});
+
 	// Add anchor for broken ARIA reference error. Not a real solution, quick and dirty, but see here: https://github.com/react-bootstrap/react-bootstrap/issues/1827
 	$('<a id="popup-anchor" href="#" class="visually-hidden">Popup</a>').appendTo('#popup');
 	
@@ -53,34 +81,45 @@ jQuery(document).ready(function($) {
 	$(window).resize(resizeChecks);
 	resizeChecks();
 	
-	$('#cu-header .cu-brand').attr('data-spy','affix');
-		
-	$('#cu-header .cu-brand').affix({
-		offset: {     
-		  top: $('#cu-header .cu-brand').offset().top
-		}
-	});
+	
 	
 	// Window Load ------------------------------------------------------------
 	$(window).on("load", function() {
+		
+		if (window.matchMedia('(max-width: 767px)').matches) {
+			$('body').removeClass('scrolled');
+		} 
 
 		$("#cu-search-form").attr("action", "/search/node/");
 		$("#cu-search-query").attr("name", "keys");
 
 		$('input[type=radio][name=sitesearch]').on('change', function() {
-	     switch($(this).val()) {
-	         case 'thissite':
-				 $("#cu-search-form").attr("action", "/search/node/");
-				 $("#cu-search-query").attr("name", "keys");
-				 cu-search-query
-	             break;
-	         case 'cornell':
-	             $("#cu-search-form").attr("action", "https://cornell.edu/search/");
-	             $("#cu-search-query").attr("name", "q");
-	             break;
-	     }
+			 switch($(this).val()) {
+				 case 'thissite':
+					 $("#cu-search-form").attr("action", "/search/node/");
+					 $("#cu-search-query").attr("name", "keys");
+					 cu-search-query
+					 break;
+				 case 'cornell':
+					 $("#cu-search-form").attr("action", "https://cornell.edu/search/");
+					 $("#cu-search-query").attr("name", "q");
+					 break;
+			 }
 		});
-
+	
 	});
+	
+	// Window scroll ------------------------------------------------------------
+	$(window).on("scroll", function() {
+		if ( $(window).width() >= 768 ) {
+			var top_offset = $(window).scrollTop();
+			if (top_offset == 0) {
+				$('body').removeClass('scrolled');
+			} else {
+				$('body').addClass('scrolled');
+			}
+		}
+	});
+	
 	
 });
